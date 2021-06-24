c73f3aba58db7556d667cd3410867a9389b8697a
aleksa <aleksa.zdravkovic1996@gmail.com>
2021-06-24 21:22:58 +0200

    [minor] small fix

--- src/app/components/game/card/index.js
+++ src/app/components/game/card/index.js
@@ -8,18 +8,18 @@ import {
 const Card = ({ id, image, showImage, matched }) => {
   const dispatch = useDispatch();
   let timeOut = null;
-  const handleClick = () => {
+  const handleClick = (id) => {
     clearInterval(timeOut);
     dispatch(turnCardUp(id));
     dispatch(validateMatchingPair());
     setTimeout(() => {
       dispatch(validateNonMatchingPair());
-    }, 2500);
+    }, 4000);
   };
   let numClicksWithinTurn = useSelector((state) => state.numClicksWithinTurn);
   const imageSource = `${window.location.origin}/images/${image}.jpg`;
   const backPath = `${window.location.origin}/images/back_of_the_card.jpg`;
-  console.log(numClicksWithinTurn);
+
   let className = "card flip-card";
   if (matched) {
     className = className + " matched";
@@ -30,7 +30,7 @@ const Card = ({ id, image, showImage, matched }) => {
     <div
       onClick={() => {
         if (!matched && !showImage && numClicksWithinTurn !== 2) {
-          handleClick();
+          handleClick(id);
         }
       }}
       className={showImage ? classNameWithAnimation : className}
